2e33fa111b2bfbc1d5e948f9449c9e980b5e8a62
Aaron Batilo <AaronBatilo@gmail.com>
2025-08-17 08:45:57 -0600

    Add update-cluster-queue configuration flag
    Add FlagUpdateClusterQueue constant to enable optional updating of
    ClusterQueue resource quotas based on calculated node capacities.
    This allows the tool to sync Kueue ClusterQueues with actual cluster
    capacity when enabled.

--- cmd/kueuecapacity/config.go
+++ cmd/kueuecapacity/config.go
@@ -1,9 +1,10 @@
 package main
 
 const (
-	FlagVerbose       = "verbose"
-	FlagLabelSelector = "label-selector"
-	FlagKubeconfig    = "kubeconfig"
-	FlagResources     = "resources"
-	FlagClusterQueue  = "cluster-queue"
+	FlagVerbose            = "verbose"
+	FlagLabelSelector      = "label-selector"
+	FlagKubeconfig         = "kubeconfig"
+	FlagResources          = "resources"
+	FlagClusterQueue       = "cluster-queue"
+	FlagUpdateClusterQueue = "update-cluster-queue"
 )
